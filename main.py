954af4f6f52ee6355e41125845cf81b7e3cbdf1a
Ganeshwara Hananda <ganesh@vaticle.com>
2025-12-17 12:08:07 +0000

    wip

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastmcp import FastMCP
 
+mcp = FastMCP("TypeDB MCP Server")
+
 @mcp.tool
 def add(num1: int, num2: int) -> int:
     return num1 + num2
 
 
-def main():
-    print("Hello from typedb-mcp!")
-    server = FastMCP[Any]("Demo")
-    server.run()
-    
-
 if __name__ == "__main__":
-    main()
+    mcp.run()
