bdc1b86a320b865f5b94d82ef85942d2dadffcc6
Ganeshwara Hananda <ganesh@vaticle.com>
2025-12-17 12:12:34 +0000

    wip

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("TypeDB MCP Server")
+mcp = FastMCP(
+    name="TypeDB MCP Server",
+    instructions="TODO",
+    version="TODO",
+    website_url="TODO",
+
+    )
 
 @mcp.tool
 def add(num1: int, num2: int) -> int:
     return num1 + num2
+
+
+@mcp.tool
+def subtract(num1: int, num2: int) -> int:
+    return num1 - num2
 
 
 if __name__ == "__main__":
