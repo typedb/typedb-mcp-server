66b932438d2676ae95a03b9f517b919d4f6ea0ab
Ganeshwara Hananda <ganesh@vaticle.com>
2025-12-18 16:10:38 +0000

    wip

--- server.py
+++ server.py
@@ -1,25 +1,25 @@
 from fastmcp import FastMCP
 
 mcp = FastMCP(
-    "Math Tools",
-    description="Provides mathematical operations like addition, subtraction, etc."
+    "TypeDB MCP Server",
+    description="Provides query capability against a TypeDB server"
 )
 
 @mcp.tool
-def add(a: float, b: float) -> float:
-    """Add two numbers together and return the result.
+def query(query: str, database: str) -> str:
+    f"""Executes given query against the given database.
     
     Args:
-        a: First number (e.g., 5.0, -3.14)
-        b: Second number (e.g., 10, 2.5)
+        query: TypeQL query to be executed
+        database: The name of the database against which the query will be executed
     
     Returns:
-        The arithmetic sum of a and b
+        query answers
     
     Example:
-        add(5, 3) returns 8
+        query("match $p isa user; get;", "users")"
     """
-    return a + b
+    return "henlo" # TODO: query typedb database
 
 if __name__ == "__main__":
     mcp.run(transport="http", host="0.0.0.0", port=8000)
